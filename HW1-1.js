5138bbd88da5c4e406b44f94ff2baea9612fc32d
Mehran Halimi <mehran.net1@gmail.com>
2013-06-26 00:13:00 +0800

    HW1-1-01

---
+++ HW1-1.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+var outfile = "prime.txt";
+var sieve = [], i, j, primes = "";
+
+for (i = 2; i <= 100; ++i) {
+    if (!sieve[i]) {
+        primes = primes + i;
+        if (i < 100)
+            primes = primes + ",";
+
+        for (j = i << 1; j <= max; j += i) {
+            sieve[j] = true;
+        }
+    }
+}
+
+fs.writeFileSync(outfile, primes);
+console.log("Script: " + __filename + "\nWrote: " + out + "To: " + outfile);
