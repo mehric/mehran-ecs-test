2e70a730aa7af7d1cc46b63fb4d0ba7318951959
Mehran Halimi <mehran.net1@gmail.com>
2013-06-26 00:24:22 +0800

    HW1-1-04

--- HW1-1.js
+++ HW1-1.js
@@ -4,15 +4,14 @@ var sieve = [], i, j, primes = "";
 
 for (i = 2; i <= 100; ++i) {
     if (!sieve[i]) {
-        primes = primes + i;
-        if (i < 100)
-            primes = primes + ",";
+        primes = primes + i + ",";
 
         for (j = i << 1; j <= 100; j += i) {
             sieve[j] = true;
         }
     }
 }
+primes = primes.substring(0, primes.length - 1)
 
 fs.writeFileSync(outfile, primes);
 console.log("Script: " + __filename + "\nWrote: " + primes + "To: " + outfile);
